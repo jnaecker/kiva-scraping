3e97fc8c6a603b60194f57e3f1549cd8abd7577e
Jeff Naecker <jnaecker@gmail.com>
2018-08-31 09:51:48 -0700

    Added sleep to spider script to avoid getting blocked

--- kiva-spider.sh
+++ kiva-spider.sh
@@ -28,6 +28,10 @@ while read LOAN; do
   # pull in the HTML for that loan and save with timestamp as filename
   curl -H "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.89 Safari/537.36" -s https://www.kiva.org/lend/${LOAN} -o "loans/${LOAN}/$(date +%Y-%m-%d-%H-%M-%S).html"
 
+  # wait before doing next curl to avoid getting blocked
+  sleep 30
+
+
 done < loan-list.txt # note the loan-list.txt is slurped here
 
 echo "Download completed at $(date +%Y-%m-%d\ %H:%M:%S)" >> $LOGFILE 2>&1
